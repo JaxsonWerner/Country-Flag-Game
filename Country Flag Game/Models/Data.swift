6b3ff80b9f36361865dfb52b18be58549ff1a7c2
Jaxson <Werner>
2026-01-15 10:42:03 -0600

    Created initial view

---
+++ Country Flag Game/Models/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Jaxson Werner on 1/12/26.
+//
+
+import Foundation
---
+++ Country Flag Game/Models/GameManager.swift
@@ -0,0 +1,8 @@
+//
+//  GameManager.swift
+//  Country Flag Game
+//
+//  Created by Jaxson Werner on 1/12/26.
+//
+
+import Foundation
